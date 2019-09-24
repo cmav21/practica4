5455d2eee4182b9d6c847893e54251daa934b45d
dcl17n15 <dcl17n15@iMac-de-administrador-2.local>
2019-09-24 09:02:26 -0500

    add build

---
+++ build/precache-manifest.fd90d6c9694d4f40883744020f4c4604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd062c0a328c0919d0aea2066ab3ea83",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbd363ba91131e06a2ad",
+    "url": "./static/css/main.35b498f3.chunk.css"
+  },
+  {
+    "revision": "82f9a391002335071741",
+    "url": "./static/js/2.33e12b2a.chunk.js"
+  },
+  {
+    "revision": "cbd363ba91131e06a2ad",
+    "url": "./static/js/main.9579a32a.chunk.js"
+  },
+  {
+    "revision": "32ca80ff0ffe20b793f9",
+    "url": "./static/js/runtime~main.329a5646.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.458f04f4b61b2239e4dc05317c086ecc.js"
+  "./precache-manifest.fd90d6c9694d4f40883744020f4c4604.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
